6ce2395ad52951d2c52b7f32a3e5b6c5a73cd7f0
Maksym-Bohun <maks.boggun@gmail.com>
2023-09-20 21:38:26 +0300

    implemented storing in db

--- BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let myName = storedName as? String{
+            nameLabel.text = "Name: \(myName)"
+        }
+        
+        if let myBirthday = storedBirthday as? String{
+            birthdayLabel.text = "Birthday: \(myBirthday)"
+        }
     }
 
-
+    @IBAction func saveClicked(_ sender: Any) {
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text, forKey: "birthday")
+        
+        nameLabel.text = "Name: \(nameTextField.text!)"
+        birthdayLabel.text = "Birthday: \(birthdayTextField.text!)"
+    }
+    
 }
 
